1c60861eae1370a77fb2ecfcc6707ef9a2fb5334
kineticvenom <kalebvarnes@yahoo.com>
2022-06-27 09:58:20 -1000

    completed implimentation

--- create_schema.sql
+++ create_schema.sql
@@ -7,3 +7,25 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id          serial PRIMARY KEY,
+  line_1      varchar(255) not NULL,
+  city        varchar(255) not NULL,
+  state       varchar(255) not NULL,
+  zipcode     integer not NULL
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes(
+  id           serial PRIMARY KEY,
+  name         varchar(255) NOT NULL,
+  credits      integer not NULL
+);
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments(
+  id           serial PRIMARY KEY,
+  student_id   integer not NULL,
+  class_id     integer not NULL,
+  grade        character(1)
+);
